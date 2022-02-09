58e408076037f2e6a629099e1e13d683b12a1c2b
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-02-09 09:08:32 +0300

    Change class: 03

---
+++ Projects/Hello/src/main.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+
+   procedure Print_Greeting is
+
+   begin
+      Put_Line("Hello madafakas!!!");
+   end Print_Greeting;
+
+begin
+   Print_Greeting;
+   null;
+end Main;
